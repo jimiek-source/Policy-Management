fc59e6bce552ce8d7affd07301a1d2695f6276fb
jimiek-source <kotebob10@gmail.com>
2025-02-21 11:59:44 +0200

    Add files via upload

---
+++ policy holder.py
@@ -0,0 +1,29 @@
+def_init_(self, policyholder_id, name, email):
+self.policyholder_id=policyholder_id
+self.name =Name
+self.email =email
+self.status ='Active' # Default status is active
+self.purchased_products []
+
+def register(self):
+    print(f"Policyholder {self.name} registered")
+
+    def suspend(self)
+        self.status +'Suspended'
+        print(f"Policyholder {self.name} suspended")
+
+        def reactivated(self);
+            self.status ='Active'
+            print(f"Policyholder {self.name} reactivated")
+
+            def add_product(self, product):
+                self.purchased_products.append(product)
+                print(f"Product {product.name} added to {self.name}'s account")
+
+                def display_account_details(self):
+                    print(f"Policyholder:(self.name")
+                    print(f"Email:{self.email}")
+                    print(f"Status.status")
+                    print(f"Purchased Products: {', '.join([product.name for product in self.purchased_products])}").
+
+    
